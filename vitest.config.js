029aeaa5a84981dcf03508938cfc237b42a974e7
ganesh <ganeshkumar159th@gmail.com>
2026-07-02 07:30:02 +0530

    adding the ci test part

--- vitest.config.js
+++ vitest.config.js
@@ -6,7 +6,19 @@ export default defineConfig({
   test: {
     environment: 'happy-dom',
     globals: true,
-    setupFiles: ['./vitest.setup.js']
+    setupFiles: ['./vitest.setup.js'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html', 'clover'],
+      include: ['src/**/*.{js,jsx}'],
+      thresholds: {
+        perFile: true,
+        statements: 95,
+        branches: 95,
+        functions: 95,
+        lines: 95,
+      }
+    }
   },
   server: {
     deps: {
